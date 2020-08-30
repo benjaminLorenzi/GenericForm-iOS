c42076a5c4dd4689c4d0933769fbcc171c537ac5
Benjamin LORENZI <lorenzibenjamin@gmail.com>
2020-08-30 19:22:20 +0200

    Image modif

--- GenericFormTests/GenericFormTests.swift
+++ GenericFormTests/GenericFormTests.swift
@@ -9,26 +9,197 @@
 import XCTest
 @testable import GenericForm
 
-class GenericFormTests: XCTestCase {
+class MockTextField: TextFieldValidatorAdaptble {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var _text: String?
+    func performValidation() {
+        
+    }
+    
+    var isEmpty: Bool {
+        return _text ?? "" == ""
+    }
+    var validators: [Validators] = []
+
+    var isValid: Bool {
+        let (isValid, _) = performValidator()
+        return isValid
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    var messages: [String] {
+        let (_, messages) = performValidator()
+        return messages
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func performValidator() -> (Bool, [String]) {
+        let (isValid, messages) = validators.applyAll(self)
+        return (isValid, messages)
     }
 
 }
+
+class GenericFormTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+
+        let mockTextField = MockTextField()
+        mockTextField.validators = [.required, .email]
+
+        mockTextField._text = "BobyLeMEc"
+
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "Invalid email format")
+
+        mockTextField._text = "BobyLeMEc@gmail.com"
+
+        XCTAssert(mockTextField.isValid == true)
+        XCTAssert( mockTextField.messages.count == 0)
+
+        mockTextField.validators = [ .regularExpression(message: "No special caracateres and between 5-10", regexp:  try! NSRegularExpression(pattern: "^[a-zA-Z0-9]{5,10}$", options: []))]
+
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "No special caracateres and between 5-10")
+
+        mockTextField.validators = [ .pattern("^[a-zA-Z0-9]{5,10}$", message: "No special caracteres and between 5-10 caracteres")]
+
+
+        mockTextField._text = "bobyJo"
+        XCTAssert(mockTextField.isValid == true)
+        XCTAssert( mockTextField.messages.count == 0)
+
+        mockTextField._text = "bobyJo@"
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "No special caracteres and between 5-10 caracteres")
+
+        mockTextField._text = "bobyJo$"
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "No special caracteres and between 5-10 caracteres")
+
+        mockTextField._text = "bobyJoasdsadasdasdas"
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "No special caracteres and between 5-10 caracteres")
+
+        mockTextField.validators = [ .required, .custom({ textField in
+            let isValid = !(textField._text ?? "").contains("benjamin")
+            return (isValid, isValid ? [] : ["i don't like benjamin"])
+        })]
+
+        mockTextField._text = "saasdasdasbenjaminsdsad"
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+        XCTAssert( mockTextField.messages[0] == "i don't like benjamin")
+
+        mockTextField._text = "dsdasdasdasdasdasdsa"
+        XCTAssert(mockTextField.isValid == true)
+        XCTAssert( mockTextField.messages.count == 0)
+
+        mockTextField._text = ""
+        XCTAssert(mockTextField.isValid == false)
+        XCTAssert( mockTextField.messages.count == 1)
+
+        let mockUserNameTextField = MockTextField()
+        mockUserNameTextField.validators = [
+            .required,
+            .minLength(length: 5, message: "needs more that 5 characters!"),
+            .maxLength(length: 25, message: "needs less that 25 characters!"),
+            first(.custom({ neoTextField in
+                guard let text = neoTextField._text else {
+                    return (false, [])
+                }
+                let isEmail = text.isValidEmail()
+                return (!isEmail, isEmail ? ["User should not be an email"] : [])
+            }), .pattern("^[a-zA-Z0-9]*$", message: "The user should not have special characters"))
+        ]
+
+        mockUserNameTextField._text = "BobyLeMEc"
+        XCTAssert(mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 0)
+
+        mockUserNameTextField._text = "BobyLeMEc$"
+        XCTAssert(!mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 1)
+        XCTAssert(mockUserNameTextField.messages[0] == "The user should not have special characters")
+
+        mockUserNameTextField._text = "BobyLeMEc@kds.com"
+        XCTAssert(!mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 1)
+        XCTAssert(mockUserNameTextField.messages[0] == "User should not be an email")
+
+        mockUserNameTextField._text = "blo&"
+        XCTAssert(!mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 2)
+        XCTAssert(mockUserNameTextField.messages[0] == "needs more that 5 characters!")
+        XCTAssert(mockUserNameTextField.messages[1] == "The user should not have special characters")
+
+        mockUserNameTextField._text = "bloasdasdasdasdaawddwdqqqwwdqwds"
+        XCTAssert(!mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 1)
+        XCTAssert(mockUserNameTextField.messages[0] == "needs less that 25 characters!")
+
+        mockUserNameTextField._text = "bloasdasdasdasdaawddwdqqqwwdqwdsˆ"
+        XCTAssert(!mockUserNameTextField.isValid)
+        XCTAssert(mockUserNameTextField.messages.count == 2)
+        XCTAssert(mockUserNameTextField.messages[0] == "needs less that 25 characters!")
+        XCTAssert(mockUserNameTextField.messages[1] == "The user should not have special characters")
+
+        let mobileNumberTextField = MockTextField()
+        mobileNumberTextField.validators = [.required, .mobileNumber]
+        mobileNumberTextField._text = "603903594"
+        XCTAssert(mobileNumberTextField.isValid)
+        XCTAssert(mobileNumberTextField.messages.count == 0)
+
+        mobileNumberTextField._text = "60390r3594"
+        XCTAssert(!mobileNumberTextField.isValid)
+        XCTAssert(mobileNumberTextField.messages[0] == "Invalid phone number")
+
+        let passwordTextField = MockTextField()
+        let passwordConfirmation = MockTextField()
+        passwordTextField.validators = [.required, .password]
+
+        func validatorPasswordMatch(_ neoTextField: TextFieldValidatorAdaptble) -> (Bool, [String]) {
+            let passwordsMatch = passwordTextField._text ==
+                passwordConfirmation._text
+            return (passwordsMatch, passwordsMatch ? [] :["Password don't match"])
+        }
+        passwordConfirmation.validators = [.required, .custom(validatorPasswordMatch)]
+
+        passwordTextField._text = "lechatestmort"
+        XCTAssert(!passwordTextField.isValid)
+        XCTAssert(passwordTextField.messages.count == 2)
+        XCTAssert(passwordTextField.messages[0] == "Use at least one number and one letter")
+        XCTAssert(passwordTextField.messages[1] == "Use at least one special character and no spaces")
+
+        passwordTextField._text = "lechatestmort3"
+        XCTAssert(!passwordTextField.isValid)
+        XCTAssert(passwordTextField.messages.count == 1)
+        XCTAssert(passwordTextField.messages[0] == "Use at least one special character and no spaces")
+
+        passwordTextField._text = "lechatestmort3@"
+        XCTAssert(passwordTextField.isValid)
+        XCTAssert(passwordTextField.messages.count == 0)
+
+        XCTAssert(!passwordConfirmation.isValid)
+        XCTAssert(passwordConfirmation.messages.count == 2)
+        XCTAssert(passwordConfirmation.messages[1] == "Password don\'t match")
+
+        passwordConfirmation._text = "lechatestmort3@"
+        XCTAssert(passwordConfirmation.isValid)
+        XCTAssert(passwordConfirmation.messages.count == 0)
+
+    }
+
+}
+
--- README.md
+++ README.md
@@ -2,9 +2,80 @@
 
 Generic Form : a generic implementation of a form 
 
+## Validator 
+
+Validators is an enum with severals utils functions that you can use :
+- required 
+- regular expression
+- email
+- password
+
+Example of usage : 
+```swift
+nameField.validators = [.required]
+emailField.validators = [.required, .email]
+```
+
+## Play with validators
+
+You should take a look at the unit tests to understand what is the behaviour of a TextField depending to a validator. 
+Here there is a MockTextField  that have two variables : "isValid" and "messages" that are computed properties. 
+It will call the validator associated functions. 
+In this example : "BobyLeMec" is not an email and should trigger a error message and "BobyLeMec@gmail.com" on the other hand is valid. 
+
+```swift
+func testExample() {
+       let mockTextField = MockTextField()
+       mockTextField.validators = [.email]
+
+       mockTextField._text = "BobyLeMEc"
+
+       XCTAssert(mockTextField.isValid == false)
+       XCTAssert( mockTextField.messages.count == 1)
+       XCTAssert( mockTextField.messages[0] == "Invalid email format")
+       
+       mockTextField.text = "BobyLeMEc@gmail.com"
+
+       XCTAssert(mockTextField.isValid == true)
+       XCTAssert( mockTextField.messages.count == 0)
+}
+```
+
+## Override the localizable strings for the generic validators 
+
+You can override the localizable strings that are used by the generic validators (.required, .email ...)
+
+```swift
+"field_empty_error" = "This field should not be empty";
+"email_format_invalid_error" = "Invalid email format";
+"mobile_number_invalid" = "Invalid phone number";
+"specials_characters" = "Use at least one special character and no spaces";
+"password_has_wrong_length" = "Password must be 8–20 characters";
+"password_number_letter_suggestion" = "Use at least one number and one letter";
+```
+
+## Write your own validation logic 
+
+<img src="customEx.png" with="100" height= "100">
+
+You can write your own validation logic by using the .custom value in the Validator enum.
+It allows you to write a closure that is going to be use inside the validation logic. 
+It should return a tuple (Bool, [String]) where the bool argument say if the field is valid and thee second return the errors messages. 
+In this example : It will detect that the textField contains "benjamin" and trigger and error message if so. 
+
+```swift
+
+nameField.validators = [.custom({ textField in
+    let isValid = !(textField._text ?? "").contains("benjamin")
+    return (isValid, isValid ? [] : ["i don't like benjamin"])
+})
+]
+
+```
+
 ## Example of usage in a ViewController 
 
-<img src="formExample.png" with="800" height= "800">
+<img src="formExample.png" with="400" height= "400">
 
 ```swift
 
@@ -58,36 +129,3 @@ class ViewController : UIViewCoordinable {
 
 ```
 
-## Write your own validation logic 
-
-<img src="customEx.png" with="300" height= "300">
-
-```swift
-
-nameField.validators = [.custom({ textField in
-    let isValid = !(textField._text ?? "").contains("benjamin")
-    return (isValid, isValid ? [] : ["i don't like benjamin"])
-})
-]
-
-```
-Validators is an enum with severals utils functions that you can use :
-- required 
-- regular expression
-- email
-- password
-....
-You can use the custom logic function if you need to write your own business logic. 
-It use a closure with a textField as parameter.
-It should return a tuple (Bool, [String]) where the bool argument say if the field is valid and thee second return the errors messages. 
-
-## Override the localizable strings for the generic validators 
-
-```swift
-"field_empty_error" = "This field should not be empty";
-"email_format_invalid_error" = "Invalid email format";
-"mobile_number_invalid" = "Invalid phone number";
-"specials_characters" = "Use at least one special character and no spaces";
-"password_has_wrong_length" = "Password must be 8–20 characters";
-"password_number_letter_suggestion" = "Use at least one number and one letter";
-```
